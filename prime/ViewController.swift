a3e4ba538b7c4ae9750cacb17cf8e5adf8d9542c
422Mac01 <422mac01@422Mac01s-iMac.local>
2017-07-27 11:54:47 +0700

    CheckSpace

--- prime/ViewController.swift
+++ prime/ViewController.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let strAlertHaveSpace = "please Fill Number"
+    
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func checkButton(_ sender: Any) {
+        
+        let strNumberFromTextField  = numberTextField.text!
+        print("Number ==> \(strNumberFromTextField)")
+        
+        if strNumberFromTextField != ""{
+           
+            print("No Space")
+        } else{
+         print("Have Space")
+            resultLabel.text = strAlertHaveSpace
+        }
+        
+    } //checkButton
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
